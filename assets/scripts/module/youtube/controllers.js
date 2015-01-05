e2fec821bd46064d503cb165e1b4d99ec510af3d
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-01-05 14:58:36 -0300

    Refactoring controllers.js, now parseData isn't a global function

--- assets/scripts/module/youtube/controllers.js
+++ assets/scripts/module/youtube/controllers.js
@@ -15,41 +15,41 @@
 		$scope.$watch('query', function(data) {
 			YoutubeService.getUserVideos(data)
 			.success(function(data) {
-				$scope.parseData(data.feed.entry);
+          parseData(data);
 
 			}).error(function(error) {
 				console.log('Error:', error);
 			});
 		});
 
-		$scope.parseData = function(data) {
-            if (data.length > 0) {
-                $scope.videos = [];
-                
-                data.forEach(function(element, index) {
-                    var video = {
-                       title: element.title.$t,
-                       link: element.link[0].href,
-                       viewCount: element.yt$statistics.viewCount,
-                       category: element.media$group.media$category[0].label,
-                       images: {
-                           cover: {
-                                src: element.media$group.media$thumbnail[0].url,
-                                height: element.media$group.media$thumbnail[0].height,
-                                width: element.media$group.media$thumbnail[0].width
-                           },
-                           thumbnail: {
-                                src: element.media$group.media$thumbnail[1].url,
-                                height: element.media$group.media$thumbnail[1].height,
-                                width: element.media$group.media$thumbnail[1].width
-                           }   
-                       }
-                    };
-                    
-                    $scope.videos.push(video);
-                });
-            }
-        };
-	}
+    function parseData(data) {
+        if (data.feed.entry.length > 0) {
+            $scope.videos = [];
+
+            data.feed.entry.forEach(function(element, index) {
+                var video = {
+                     title: element.title.$t,
+                     link: element.link[0].href,
+                     viewCount: element.yt$statistics.viewCount,
+                     category: element.media$group.media$category[0].label,
+                     images: {
+                         cover: {
+                              src: element.media$group.media$thumbnail[0].url,
+                              height: element.media$group.media$thumbnail[0].height,
+                              width: element.media$group.media$thumbnail[0].width
+                         },
+                         thumbnail: {
+                              src: element.media$group.media$thumbnail[1].url,
+                              height: element.media$group.media$thumbnail[1].height,
+                              width: element.media$group.media$thumbnail[1].width
+                         }   
+                     }
+                  };
+                  
+                  $scope.videos.push(video);
+            });
+        }
+    }
+  }
 	
 })();
