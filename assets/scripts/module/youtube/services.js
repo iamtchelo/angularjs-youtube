c5ddf39dd7ee8f9604062c61dd6f36305c0db505
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-01-12 18:14:43 -0300

    Add singleVideo url api, preparing to create the modal #1

--- assets/scripts/module/youtube/services.js
+++ assets/scripts/module/youtube/services.js
@@ -9,7 +9,8 @@
 	function YoutubeService($http) {
 		var YoutubeAPI = {
 			defaultParams: '&orderBy=published&start-index=1',
-	        userUploads: 'http://gdata.youtube.com/feeds/api/users/{username}/uploads?alt=json'
+	        userUploads: 'http://gdata.youtube.com/feeds/api/users/{username}/uploads?alt=json',
+	        singleVideo: 'https://gdata.youtube.com/feeds/api/videos/{videoid}?alt=json'
 	    };
 
 	    return {
