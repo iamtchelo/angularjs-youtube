0785ca8f0956e741ead2468923fb751a121585d5
Marcelo Silva <marcelosilva.developer@gmail.com>
2015-01-16 11:16:30 -0300

    Add get all videos url api by keyword

--- assets/scripts/module/youtube/services.js
+++ assets/scripts/module/youtube/services.js
@@ -10,13 +10,24 @@
 		var YoutubeAPI = {
 			defaultParams: '&orderBy=published&start-index=1',
 	        userUploads: 'http://gdata.youtube.com/feeds/api/users/{username}/uploads?alt=json',
-	        singleVideo: 'https://gdata.youtube.com/feeds/api/videos/{videoid}?alt=json'
+	        singleVideo: 'https://gdata.youtube.com/feeds/api/videos/{videoid}?alt=json',
+	        videos: 'http://gdata.youtube.com/feeds/api/videos?q={keyword}&alt=json'
 	    };
 
 	    return {
 	    	getUserVideos: function(query) {
 	    		var url = YoutubeAPI.userUploads.replace(/(\{username\})/g, query);
 	    		return $http.get(url + YoutubeAPI.defaultParams);
+	    	},
+
+	    	getVideo: function(id) {
+	    		var url = YoutubeAPI.singleVideo.replace(/(\{videoid\})/g, id);
+	    		return $http.get(url);
+	    	},
+
+	    	getVideos: function(query) {
+	    		var url = YoutubeAPI.videos.replace(/(\{keyword\})/g, query);
+	    		return $http.get(url);
 	    	}
 	    };
 	}
